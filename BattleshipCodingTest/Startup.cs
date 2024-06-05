cdfd3ec8006ca8ca18d53484081b9e18dab36760
Ujjwal Jejurkar <Ujjwal.jejurkar@aress.com>
2024-06-05 13:03:16 +0530

    - added folders

---
+++ BattleshipCodingTest/Startup.cs
@@ -0,0 +1,63 @@
+﻿using BattleshipCodingTest.Interfaces;
+using BattleshipCodingTest.Services;
+using BattleshipCodingTest.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace BattleshipCodingTest
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddCors(c =>
+            //{
+            //    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod()
+            //     .AllowAnyHeader());
+            //});
+            //services.AddSession();
+            //services.AddMvc(options =>
+            //{
+            //    options.Filters.Add(new ResponseCacheAttribute
+            //    {
+            //        Location = ResponseCacheLocation.None,
+            //        NoStore = true
+            //    });
+            //});
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            //services.AddHttpClient();
+
+            //service registration
+            services.AddScoped<IBattleshipService, BattleshipService>();
+            //services.AddControllers().AddJsonOptions(options =>
+            //{
+            //    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            //});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BattleshipCodingTest", Version = "v1" });
+            });
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            //app.UseHttpsRedirection();
+            //app.UseRouting();
+            //app.UseCors("AllowOrigin");
+            app.UseExceptionHandlingMiddleware();
+            //app.UseSession();
+            //app.MapControllers();
+        }
+    }
+}
---
+++ BattleshipCodingUnitTest/Controllers/RestartBoard_UnitTests.cs
@@ -0,0 +1,49 @@
+﻿using BattleshipCodingTest.Controllers;
+using BattleshipCodingTest.Interfaces;
+using BattleshipCodingTest.Utilities.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace BattleshipCodingUnitTest.Services
+{
+    public class RestartBoard_UnitTests
+    {
+        public Mock<IBattleshipService> mock = new Mock<IBattleshipService>();
+
+        /// <summary>
+        /// Verifies that game properties _hitCount and _allShipsDestroyed are reset after a restart
+        /// </summary>
+        [Fact]
+        public void TestRestartGame_ResetsGameProperties()
+        {
+            //Arrange 
+            mock.Setup(p => p.RestartGame()).Returns(Constant.GameRestartSuccessfully);
+            BattleshipApiController battleship = new BattleshipApiController(mock.Object);
+
+            // Act
+            var response = battleship.RestartGame() as OkObjectResult;
+
+            //assert
+            Assert.IsType<OkObjectResult>(response);
+            Assert.Equal(Constant.GameRestartSuccessfully, response.Value);
+        }
+
+        /// <summary>
+        /// Verifies that the method throws the correct exception when the board does not exist
+        /// </summary>
+        [Fact]
+        public void TestRestartGame_BoardNotExist()
+        {
+            //Arrange 
+            mock.Setup(p => p.RestartGame()).Returns(Constant.BoardNotExist);
+            BattleshipApiController battleship = new BattleshipApiController(mock.Object);
+
+            // Act
+            var response = battleship.RestartGame() as OkObjectResult;
+
+            //assert
+            Assert.IsType<OkObjectResult>(response);
+            Assert.Equal(Constant.BoardNotExist, response.Value);
+        }
+    }
+}
